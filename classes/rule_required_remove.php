d2a304962251525e109e6b4985ef09e5550e7be7
Geoffrey van Wyk <geoffrey@ttro.com>
2022-12-06 16:51:16 +0200

    lint: Expected 1 space before opening brace

--- classes/rule_required_remove.php
+++ classes/rule_required_remove.php
@@ -30,7 +30,7 @@ namespace profilefield_conditional;
  * @copyright  2016 Shamim Rezaie {@link http://foodle.org}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class rule_required_remove extends \HTML_QuickForm_Rule  {
+class rule_required_remove extends \HTML_QuickForm_Rule {
     /**
      * Checks if an element is not empty.
      * This is a server-side validation, it works for both text fields and editor fields
